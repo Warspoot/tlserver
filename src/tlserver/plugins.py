45d274291e33bd934374cc110189ddaf7394cae4
de-odex <de-odex@users.noreply.github.com>
2025-10-10 20:33:49 +0900

    disable the "plugin" for now
    need a better solution...

--- src/tlserver/plugins.py
+++ src/tlserver/plugins.py
@@ -4,15 +4,6 @@ import re
 def filter_text(extracted_text: str) -> str:
     result = extracted_text
 
-    result = result.replace("\n", "<br>")
-    result = result.replace("{", "")  # remove { sign in all positions of the text
-    result = result.replace("�", "")  # remove the first unknown sign in the text
-    result = re.sub(r"カ$", "", result)  # remove カ symbol at the end of the text
-    result = re.sub(
-        r"987$", "?", result
-    )  # replace number 987 at the end of the text with ?
-    result = re.sub(r"^:", "", result)  # remove colon at the beginning of the text
-
     return result  # noqa: RET504
 
 
